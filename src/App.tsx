63d1d789f19901828f5940ecdb5fe081fdb81b0c
Roman Martiník <r.martinik@gmail.com>
2022-03-19 12:14:55 +0100

    fasdfsfd

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          Learn React! bomby
         </a>
       </header>
     </div>
