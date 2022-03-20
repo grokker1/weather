0438e7d08b5abdf2a5a7eca2455f9b024765bb73
Roman Martiník <r.martinik@gmail.com>
2022-03-20 13:22:32 +0100

    Before weather card components

--- src/Components/fetch.tsx
+++ src/Components/fetch.tsx
@@ -3,20 +3,23 @@ const Fetch = () => {
   const [longtitude, setLongtitude] = useState(0);
   const [latitude, setLatitude] = useState(0);
   const [weatherData, setWeatherData] = useState({});
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       setLongtitude(position.coords.longitude);
       setLatitude(position.coords.latitude);
     });
+
     fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longtitude}&exclude=hourly,daily&appid=4d20d8eee158a891dbf179a0c800b680
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longtitude}&exclude=hourly,daily,minutely&appid=4d20d8eee158a891dbf179a0c800b680
       `
     )
-      .then((response) => response.json())
+      .then((data) => data.json())
       .then((data) => {
         setWeatherData(data);
         console.log(data);
       });
+
     console.log(latitude + longtitude);
   }, [longtitude, latitude]);
 
